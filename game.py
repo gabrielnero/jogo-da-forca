b05b788e8694da37cc8648f2560ef660998411bd
Gabriel <83769003+gabrielnero@users.noreply.github.com>
2022-11-06 20:21:03 -0300

    Update game.py

--- game.py
+++ game.py
@@ -1,22 +1,26 @@
+# Importar ferramentas
 import os
-from util import configura_jogo, gerar_palavra_secreta, exibir_jogo, exibir_jogo_sem_dica, mostrar_dica, comparar_chute, op_invalida, salva_partida, exibe_historico, jogar_novamente
+from util import configura_jogo, gerar_palavra_secreta, exibir_jogo, exibir_jogo_sem_dica, mostrar_dica, comparar_chute, \
+    op_invalida, salva_partida, exibe_historico, jogar_novamente
 
+# Setup de variáveis
 jogar = True
 while jogar:
     nomes = True
     while nomes:
-        jogador = input("Insira o nome do jogador: ")
-        desafiante = input("Insira o nome do desafiante: ")
-        if len(jogador) >= 2 and len(desafiante) >= 2:
-            nomes = False
-        else: 
+        nome_jogador = input("Insira o nome do jogador: ").capitalize()
+        nome_desafiante = input("Insira o nome do desafiante: ").capitalize()
+        if len(nome_jogador) < 2 or len(nome_desafiante) < 2:
             os.system("cls")
             print("Os nomes devem possuir mais de 2 caracteres!")
-
+        else:
+            nomes = False
 
     os.system("cls")
-    
-    palavra_chave_dicas = configura_jogo(desafiante)
+
+# Configurações do jogo
+
+    palavra_chave_dicas = configura_jogo(nome_desafiante)
     palavra_chave = palavra_chave_dicas[0]
 
     dica_1 = palavra_chave_dicas[1]
@@ -24,7 +28,7 @@ while jogar:
     dica_3 = palavra_chave_dicas[3]
 
     rodada = True
-    
+
     os.system("cls")
 
     vencedor = ""
@@ -32,73 +36,75 @@ while jogar:
 
     dicas = [dica_1, dica_2, dica_3]
     contador_dicas = 0
-    
+
     erros = 0
-    
+
     while (rodada):
         if contador_dicas < 3:
-            exibir_jogo(palavra_chave, dica_1, dica_2, dica_3, palavra_censurada, erros)
-            op = input("Insira uma opção: ")        
+            exibir_jogo(palavra_chave, palavra_censurada, erros)
+            op = input("Insira uma opção: ")
         else:
             exibir_jogo_sem_dica(palavra_chave, palavra_censurada, erros)
             op = "2"
-        
+
         if op == "1":
             contador_dicas = mostrar_dica(dicas, contador_dicas)
             chute = input("Chute uma letra: ")
             comparacao = comparar_chute(chute, palavra_chave, palavra_censurada, erros)
             palavra_censurada = comparacao[0]
             erros = comparacao[1]
-            
+
         elif op == "2":
             chute = input("Chute uma letra: ")
             comparacao = comparar_chute(chute, palavra_chave, palavra_censurada, erros)
             palavra_censurada = comparacao[0]
             erros = comparacao[1]
-            
-        else:   
+
+        else:
             op_invalida()
             continue
 
         if erros == 5:
             rodada = False
-            print("O Desafiante {} venceu!" .format(desafiante))
+            print("O Desafiante {} venceu!".format(nome_desafiante))
             vencedor = "Desafiante"
-            
-            print("    _______________         ")
-            print("   /               \       ")
-            print("  /                 \      ")
-            print("//                   \/\  ")
-            print("\|   XXXX     XXXX   | /   ")
-            print(" |   XXXX     XXXX   |/     ")
-            print(" |   XXX       XXX   |      ")
-            print(" |                   |      ")
-            print(" \__      XXX      __/     ")
-            print("   |\     XXX     /|       ")
-            print("   | |           | |        ")
-            print("   | I I I I I I I |        ")
-            print("   |  I I I I I I  |        ")
-            print("   \_             _/       ")
-            print("     \_         _/         ")
-            print("       \_______/           ")
+            print('''
+                _______________         
+               /               \       
+              /                 \      
+            //                   \/\  
+            \|   XXXX     XXXX   | /  
+             |   XXXX     XXXX   |/     
+             |   XXX       XXX   |      
+             |                   |      
+             \__      XXX      __/     
+               |\     XXX     /|       
+               | |           | |        
+               | I I I I I I I |        
+               |  I I I I I I  |        
+               \_             _/       
+                 \_         _/         
+                   \_______/           
+            ''')
 
         elif palavra_censurada == palavra_chave.upper():
             rodada = False
-            print("O jogador {} venceu!" .format(jogador))
-            def imprime_mensagem_vencedor():
-                print("Parabéns, você ganhou!")
-        print("       ___________      ")
-        print("      '._==_==_=_.'     ")
-        print("      .-\\:      /-.    ")
-        print("     | (|:.     |) |    ")
-        print("      '-|:.     |-'     ")
-        print("        \\::.    /      ")
-        print("         '::. .'        ")
-        print("           ) (          ")
-        print("         _.' '._        ")
-        print("        '-------'       ")
-        vencedor = "Jogador"
+            print("O jogador {} venceu!".format(nome_jogador))
+            print('''        
+               ___________      
+              '._==_==_=_.'     
+              .-\\:      /-.    
+             | (|:.     |) |    
+              '-|:.     |-'     
+                \\::.    /      
+                 '::. .'        
+                   ) (          
+                 _.' '._        
+                '-------'       
+                 ''')
+            vencedor = "Jogador"
 
-    salva_partida(desafiante, jogador, palavra_chave, vencedor)
+# Histórico de partidas / finalização
+    salva_partida(vencedor, nome_desafiante, nome_jogador, palavra_chave)
     exibe_historico()
     jogar = jogar_novamente()
